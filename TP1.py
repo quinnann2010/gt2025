703464cde239e28007aaec8d142b654b0aac5405
quinnann2010 <155611767+quinnann2010@users.noreply.github.com>
2025-01-14 13:33:28 +0700

    Update TP1.py

--- TP1.py
+++ TP1.py
@@ -6,23 +6,21 @@ def path_exists(graph, start, end):
         return True
 
     visited = set()
-    queue = deque([start])  # Hàng đợi để duyệt BFS
+    queue = deque([start])  
 
     while queue:
-        current_node = queue.popleft()  # Lấy phần tử đầu hàng đợi
+        current_node = queue.popleft()  
         if current_node == end:
             return True
 
-        visited.add(current_node)  # Đánh dấu đã thăm
+        visited.add(current_node)  
 
-        # Thêm các nút kề chưa được thăm vào hàng đợi
         for neighbor in graph.get(current_node, []):
             if neighbor not in visited:
                 queue.append(neighbor)
 
     return False
 
-# Graph node
 graph = {
     '1': ['2'],
     '2': ['5'],
@@ -33,19 +31,15 @@ graph = {
     '7': []
 }
 
-# Get start node
 start = input("Start node: ").strip()
 
-# Get end node
 end = input("End node: ").strip()
 
-# Validate start and end nodes
 if start not in graph:
     print(f"Error: Start node '{start}' does not exist in the graph.")
 elif end not in graph:
     print(f"Error: End node '{end}' does not exist in the graph.")
 else:
-    # Check path exists
     if path_exists(graph, start, end):
         print(f"=> Path exists between {start} and {end}.")
     else:
